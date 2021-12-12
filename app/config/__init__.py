cbd4a558c0ac68d792e08812a46d69896a5d3f9b
gjunior <glaucioripol@gmail.com>
2021-12-12 18:22:29 -0300

    chore: update configs

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -4,6 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+def get_corret_db_string_connection():
+    if getenv('IS_DEV') == '1':
+        return getenv('DB_STRING_CONNECTION_DEV')
+    else:
+        return getenv('DB_STRING_CONNECTION_PROD')
+
+
 class Configuration:
     is_dev: bool = getenv('IS_DEV') == '1'
 
@@ -13,4 +20,4 @@ class Configuration:
     jwt_authtype: str = getenv('JWT_AUTHTYPE')
 
     # Database
-    db_string_connection: str = getenv('DB_STRING_CONNECTION')
+    db_string_connection: str = get_corret_db_string_connection()
